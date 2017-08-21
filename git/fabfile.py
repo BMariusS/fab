702df13103df1df7f99a37c2b53aa77d876e006e
root <masbrgh@gmail.com>
2017-08-21 14:11:46 +0300

    test

--- git/fabfile.py
+++ git/fabfile.py
@@ -88,7 +88,7 @@ def gitCheckout(branch):
 def find(name, path):
 	for root, dirs, files in os.walk(path):
 		if name in files:
-			print "%s" root
+			print "%s" % root
 			return os.path.join(root)
 
 
