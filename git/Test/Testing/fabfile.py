89e94ea4335e05733ab87e18c1e522d136b65a85
root <masbrgh@gmail.com>
2017-09-07 15:29:13 +0300

    xml TC,TG

---
+++ git/Test/Testing/fabfile.py
@@ -0,0 +1,26 @@
+from fabric.api import *
+import xml.etree.ElementTree as ET
+
+tree=ET.parse("variables.xml")
+root = tree.getroot()
+env.hosts=["localhost"]
+
+def testing(folderParameter='all',parameterTG='all', parameterTC='all'):
+	lista=[]
+	for folder in root.findall('folder'):
+		nameFolder = folder.get('name')
+		if nameFolder == folderParameter or folderParameter == 'all':
+			for TG in folder.findall('TG'):
+				nameTG = TG.get('name')
+				if nameTG == parameterTG or parameterTG == 'all':
+					for TC in TG.findall('TC'):
+						nameTC = TC.get('name')
+						if nameTC == parameterTC or parameterTC == 'all':
+							for TARGET in TC.findall('TARGET'):
+								for variables in TARGET.findall('variables'):
+									lista.append(variables.text)
+								variabile = ' '.join(str(i) for i in lista)
+								#print "%s %s" % (TARGET.get('id'),variabile)
+								sudo("echo '%s'" % variabile)
+								lista=[]
+
