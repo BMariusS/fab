3e2b7f4a9ced50250fc92f2f850f2709c7530daf
root <masbrgh@gmail.com>
2017-09-07 18:16:45 +0300

    logs

--- git/Test/Testing/fabfile.py
+++ git/Test/Testing/fabfile.py
@@ -1,12 +1,26 @@
 from fabric.api import *
 import xml.etree.ElementTree as ET
+from datetime import datetime
+import os
+import subprocess
+import sys
+import time
+import signal
+
 
 tree=ET.parse("variables.xml")
 root = tree.getroot()
 env.hosts=["localhost"]
+timeStart=time.time()
+logFile = "/tmp/TestLog"
 
-def testing(folderParameter='all',parameterTG='all', parameterTC='all'):
+@task
+def testing(folderParameter='all',parameterTG='all', parameterTC='all', parameterTarget='all'):
 	lista=[]
+	logTime = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+	allOpenFile = open('%s' % logFile, 'a')
+	allOpenFile.write("%s \n" % logTime)
+	allOpenFile.close()
 	for folder in root.findall('folder'):
 		nameFolder = folder.get('name')
 		if nameFolder == folderParameter or folderParameter == 'all':
@@ -17,10 +31,38 @@ def testing(folderParameter='all',parameterTG='all', parameterTC='all'):
 						nameTC = TC.get('name')
 						if nameTC == parameterTC or parameterTC == 'all':
 							for TARGET in TC.findall('TARGET'):
-								for variables in TARGET.findall('variables'):
-									lista.append(variables.text)
-								variabile = ' '.join(str(i) for i in lista)
-								print "%s %s" % (TARGET.get('id'),variabile)
-								#sudo("echo '%s %s'" % (TARGET.get('id'),variabile))
-								lista=[]
+								targetIP=TARGET.get('ip')
+								if targetIP == parameterTarget or parameterTarget == 'all':
+									for variables in TARGET.findall('variables'):
+										lista.append(variables.text)
+									variabile = ' '.join(str(i) for i in lista)
+									try:
+										pid=os.fork()
+										if pid == 0:
+											commandParameterConstruct = 'ssh %s %s' % (targetIP, variabile)
+											shellCommand = [commandParameterConstruct]
+											process = subprocess.Popen(shellCommand, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, preexec_fn=os.setsid)
+											openFile = open('%s.%s.%s' % (logFile,nameFolder,nameTC), 'a')
+											openFile.write("%s \n" % logTime)
+											openFile.close()
+											while(process.poll() == None):
+												timeNow=time.time()
+												processEndTime = int(timeNow - timeStart)
+												sys.stdout.flush()
+												openFile = open('%s.%s.%s' %(logFile, nameFolder,nameTC), 'a')
+												allOpenFile = open('%s' % logFile, 'a')
+												for line in iter(process.stdout.readline, b''):
+													print(line.rstrip())
+													openFile.write(line)
+													allOpenFile.write(line)
+													break
+												if processEndTime >= 3600:
+													os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+											return
+									except:
+										print("Error at running TC")
+										allOpenFile = open('%s' % logFile, 'a')
+										allOpenFile.write("Error at running TC")
+										
+									lista=[]
 
